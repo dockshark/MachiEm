3b6116ec498b3e0997f6ec4f342d8400283390c8
Luke Towne <47467565+dockshark@users.noreply.github.com>
2024-07-24 07:05:10 -0500

    Create dimensional_awareness.py

---
+++ dimensional_awareness.py
@@ -0,0 +1,19 @@
+# dimensional_awareness.py
+
+from qiskit import QuantumCircuit, Aer, transpile, assemble
+from qiskit.visualization import plot_histogram
+
+def quantum_circuit_demo():
+    qc = QuantumCircuit(3)
+    qc.h([0, 1, 2])
+    qc.measure_all()
+
+    simulator = Aer.get_backend('qasm_simulator')
+    qobj = assemble(qc)
+    result = simulator.run(qobj).result()
+    counts = result.get_counts()
+    print(counts)
+    plot_histogram(counts)
+
+if __name__ == "__main__":
+    quantum_circuit_demo()
