62a7a52601bcb7d41796d5e177afcba4873108ce
Luke Towne <47467565+dockshark@users.noreply.github.com>
2024-07-24 08:59:37 -0500

    Update test_dimensional_awareness.py

--- test_dimensional_awareness.py
+++ test_dimensional_awareness.py
@@ -1,4 +1,5 @@
+from qiskit_aer import Aer
 from dimensional_awareness import quantum_circuit_demo
 
 def test_quantum_circuit_demo():
-    quantum_circuit_demo()
+    assert quantum_circuit_demo() is not None
