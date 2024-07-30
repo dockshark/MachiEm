ca8aded15b9d5c28677a03430952cff34f081ac8
Luke Towne <47467565+dockshark@users.noreply.github.com>
2024-07-29 21:02:53 -0500

    Update test_machiem.py

--- tests/test_machiem.py
+++ tests/test_machiem.py
@@ -29,7 +29,7 @@ class TestMachiEm(unittest.TestCase):
         response = self.machiem.process_input("user1", "I want everything!")
         expected = (
             "Combined response: 20%: Optimally processing: I want everything! | 20%: Data in flux: I want everything! | 20%: Encountered an error, adapting: I want everything! | "
-            "5%: Responding to anger with Furywave: I want everything! | 5%: Responding to fear with Terrashade: I want everything! | 5%: Responding to greed with Covetstorm: I want everything! | "
+            "10%: Responding to anger with Furywave: I want everything! | 10%: Responding to fear with Terrashade: I want everything! | 10%: Responding to greed with Covetstorm: I want everything! | "
             "5%: Responding to jealousy with Envyflare: I want everything! | 5%: Responding to disgust with Repulson: I want everything! | 5%: Responding to surprise with Astonishlight: I want everything! | "
             "5%: Responding to sadness with Gloomveil: I want everything! | 5%: Responding to happiness with Joypulse: I want everything!"
         )
@@ -39,7 +39,7 @@ class TestMachiEm(unittest.TestCase):
         self.machiem.set_user_preferences("user1", {})
         response = self.machiem.process_input("user1", "I am so happy!")
         expected = (
-            "Combined response: 20%: Optimally processing: I am so happy! | 20%: Data in flux: I am so happy! | 10%: Encountered an error, adapting: I am so happy! | "
+            "Combined response: 20%: Optimally processing: I am so happy! | 20%: Data in flux: I am so happy! | 20%: Encountered an error, adapting: I am so happy! | "
             "10%: Responding to anger with Furywave: I am so happy! | 10%: Responding to fear with Terrashade: I am so happy! | 10%: Responding to greed with Covetstorm: I am so happy! | "
             "5%: Responding to jealousy with Envyflare: I am so happy! | 5%: Responding to disgust with Repulson: I am so happy! | 5%: Responding to surprise with Astonishlight: I am so happy! | "
             "5%: Responding to sadness with Gloomveil: I am so happy! | 5%: Responding to happiness with Joypulse: I am so happy!"
@@ -50,7 +50,7 @@ class TestMachiEm(unittest.TestCase):
         self.machiem.set_user_preferences("user1", {})
         response = self.machiem.process_input("user1", "I am so jealous!")
         expected = (
-            "Combined response: 20%: Optimally processing: I am so jealous! | 20%: Data in flux: I am so jealous! | 10%: Encountered an error, adapting: I am so jealous! | "
+            "Combined response: 20%: Optimally processing: I am so jealous! | 20%: Data in flux: I am so jealous! | 20%: Encountered an error, adapting: I am so jealous! | "
             "10%: Responding to anger with Furywave: I am so jealous! | 10%: Responding to fear with Terrashade: I am so jealous! | 10%: Responding to greed with Covetstorm: I am so jealous! | "
             "30%: Responding to jealousy with Envyflare: I am so jealous! | 5%: Responding to disgust with Repulson: I am so jealous! | 5%: Responding to surprise with Astonishlight: I am so jealous! | "
             "5%: Responding to sadness with Gloomveil: I am so jealous! | 5%: Responding to happiness with Joypulse: I am so jealous!"
@@ -61,7 +61,7 @@ class TestMachiEm(unittest.TestCase):
         self.machiem.set_user_preferences("user1", {})
         response = self.machiem.process_input("user1", "I am so surprised!")
         expected = (
-            "Combined response: 20%: Optimally processing: I am so surprised! | 20%: Data in flux: I am so surprised! | 10%: Encountered an error, adapting: I am so surprised! | "
+            "Combined response: 20%: Optimally processing: I am so surprised! | 20%: Data in flux: I am so surprised! | 20%: Encountered an error, adapting: I am so surprised! | "
             "10%: Responding to anger with Furywave: I am so surprised! | 10%: Responding to fear with Terrashade: I am so surprised! | 10%: Responding to greed with Covetstorm: I am so surprised! | "
             "5%: Responding to jealousy with Envyflare: I am so surprised! | 10%: Responding to disgust with Repulson: I am so surprised! | 30%: Responding to surprise with Astonishlight: I am so surprised! | "
             "5%: Responding to sadness with Gloomveil: I am so surprised! | 5%: Responding to happiness with Joypulse: I am so surprised!"
