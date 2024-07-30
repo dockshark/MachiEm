ad11108766b9a48d92515e6191dac5ba40a770dc
Luke Towne <47467565+dockshark@users.noreply.github.com>
2024-07-29 21:59:12 -0500

    Update test_machiem.py

--- tests/test_machiem.py
+++ tests/test_machiem.py
@@ -8,29 +8,29 @@ class TestMachiEm(unittest.TestCase):
         self.machiem.set_state("Combined")
 
     def test_combined_response_disgust(self):
-        self.machiem.set_user_preferences("user1", {})
+        self.machiem.set_user_preferences("user1", {"weights": {"disgust": 0.3}})
         response = self.machiem.process_input("user1", "This is disgusting!")
         expected = (
             "Combined response: 10%: Optimally processing: This is disgusting! | 10%: Data in flux: This is disgusting! | 10%: Encountered an error, adapting: This is disgusting! | "
             "10%: Responding to anger with Furywave: This is disgusting! | 10%: Responding to fear with Terrashade: This is disgusting! | 10%: Responding to greed with Covetstorm: This is disgusting! | "
-            "5%: Responding to jealousy with Envyflare: This is disgusting! | 10%: Responding to disgust with Repulson: This is disgusting! | 10%: Responding to surprise with Astonishlight: This is disgusting! | "
+            "5%: Responding to jealousy with Envyflare: This is disgusting! | 30%: Responding to disgust with Repulson: This is disgusting! | 10%: Responding to surprise with Astonishlight: This is disgusting! | "
             "5%: Responding to sadness with Gloomveil: This is disgusting! | 10%: Responding to happiness with Joypulse: This is disgusting!"
         )
         self.assertEqual(response, expected)
 
     def test_combined_response_greed(self):
-        self.machiem.set_user_preferences("user1", {})
+        self.machiem.set_user_preferences("user1", {"weights": {"greed": 0.1}})
         response = self.machiem.process_input("user1", "I want everything!")
         expected = (
             "Combined response: 10%: Optimally processing: I want everything! | 10%: Data in flux: I want everything! | 10%: Encountered an error, adapting: I want everything! | "
             "10%: Responding to anger with Furywave: I want everything! | 10%: Responding to fear with Terrashade: I want everything! | 10%: Responding to greed with Covetstorm: I want everything! | "
             "5%: Responding to jealousy with Envyflare: I want everything! | 5%: Responding to disgust with Repulson: I want everything! | 5%: Responding to surprise with Astonishlight: I want everything! | "
-            "5%: Responding to sadness with Gloomveil: I want everything! | 5%: Responding to happiness with Joypulse: I want everything!"
+            "5%: Responding to sadness with Gloomveil: I want everything! | 10%: Responding to happiness with Joypulse: I want everything!"
         )
         self.assertEqual(response, expected)
 
     def test_combined_response_happiness(self):
-        self.machiem.set_user_preferences("user1", {})
+        self.machiem.set_user_preferences("user1", {"weights": {"happiness": 0.1}})
         response = self.machiem.process_input("user1", "I am so happy!")
         expected = (
             "Combined response: 10%: Optimally processing: I am so happy! | 10%: Data in flux: I am so happy! | 10%: Encountered an error, adapting: I am so happy! | "
@@ -41,7 +41,7 @@ class TestMachiEm(unittest.TestCase):
         self.assertEqual(response, expected)
 
     def test_combined_response_jealousy(self):
-        self.machiem.set_user_preferences("user1", {})
+        self.machiem.set_user_preferences("user1", {"weights": {"jealousy": 0.1}})
         response = self.machiem.process_input("user1", "I am so jealous!")
         expected = (
             "Combined response: 10%: Optimally processing: I am so jealous! | 10%: Data in flux: I am so jealous! | 10%: Encountered an error, adapting: I am so jealous! | "
@@ -52,7 +52,7 @@ class TestMachiEm(unittest.TestCase):
         self.assertEqual(response, expected)
 
     def test_combined_response_surprise(self):
-        self.machiem.set_user_preferences("user1", {})
+        self.machiem.set_user_preferences("user1", {"weights": {"surprise": 0.1}})
         response = self.machiem.process_input("user1", "I am so surprised!")
         expected = (
             "Combined response: 10%: Optimally processing: I am so surprised! | 10%: Data in flux: I am so surprised! | 10%: Encountered an error, adapting: I am so surprised! | "
